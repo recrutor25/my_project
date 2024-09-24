eb37ea5fb8c364c9250d6e0d5a0c3b466e29eade
recrutor25 <sommersi@yandex.ru>
2024-09-24 20:57:46 +0200

    add 24.09

---
+++ lesson_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 17 09 2024
+@author: igor
+"""
+# 1st program
+print(5*9**0.5)
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+print(2*2+2, 2*(2+2), 2*2+2 == 2*(2+2))
+
+# 3.1 st program другой вариант
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+# 4st program
+print(int(float('123.456')*10 % 10 // 1))
