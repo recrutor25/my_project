73db80ca32fda1f1cdc8a78a4ffaedbda37f1995
recrutor25 <sommersi@yandex.ru>
2024-09-24 21:07:26 +0200

    add 24.09

---
+++ lesson_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 20 09 2024
+@author: igor
+"""
+example = 'Топинамбур'
+print(example[0])  # первый символ
+print(example[-1]) # последний символ
+print(example[5:]) # с шестого символа до конца
+print(example[::-1]) # в обратном порядке
+print(example[1::2]) # со второго символа с шагом два
