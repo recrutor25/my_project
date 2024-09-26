b072d226e28401e993b8489f817e888b8bf86861
recrutor25 <sommersi@yandex.ru>
2024-09-26 21:40:55 +0200

    add 24.09

--- module_1_7.py
+++ module_1_7.py
@@ -4,3 +4,18 @@
 Created on 22 09 2024
 @author: igor
 """
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+student_list = sorted(list(students)) # сортируем список по алфавиту
+# print(student_list)
+
+middl_grades_list = [(sum(grades[0]) / len(grades[0])),(sum(grades[1]) / len(
+    grades[1])),(sum(grades[2]) / len(grades[2])),(sum(grades[3]) / len(
+    grades[3])), (sum(grades[4]) / len(grades[4]))]  # вычисляем средний бал
+# print(grades_middl_list)
+
+middl_grades_dict = dict(zip(student_list, middl_grades_list)) # совмещаем
+# списки в соответствии с индексом
+
+print(middl_grades_dict) #len(grades[3])),
